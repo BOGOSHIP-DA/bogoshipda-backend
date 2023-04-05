ad29412cfdb382dcfc64f948ee015bbf3f33ffab
YeongT <94744740+YeongT@users.noreply.github.com>
2023-04-06 01:14:19 +0900

    init: Initial Commit

---
+++ routes/app.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import express, {json, urlencoded} from 'express';
+import path from 'path';
+
+import router from './index';
+import * as process from "process";
+
+const app = express();
+
+try {
+    const envFile = fs.statSync(path.join(__dirname, '../config/.env'));
+    dotenv.config({path: path.join(__dirname, '../config/.env')});
+} catch (err) {
+    const envFileError = err as Error;
+    if (envFileError.message.includes('ENOENT'))
+        throw new Error('missing \'.env\' file in \'config\' folder. please modify \'.env.sample\' file.');
+}
+
+app.use(cors());
+app.use(cookieParser());
+app.use(json({limit: '10mb'}));
+app.use(urlencoded({extended: true}));
+app.use(express.static(__dirname + '../public'));
+
+app.use('/', router);
+
+export {app};
---
+++ server/serve.ts
@@ -0,0 +1,24 @@
+import {createServer} from "http";
+
+//# import express app from routes folder
+import {app} from "../routes/app";
+
+// Get port from environment and store in Express.
+const port = process.env.PORT || 5000;
+app.set("port", port);
+
+const server = createServer(app);
+const onError = (error: Error) => {
+    if (error.message.includes("EACCES"))
+        throw new Error(`port ${port} requires elevated privileges`);
+    if (error.message.includes("EADDRINUSE"))
+        throw new Error(`port ${port} is already in use`);
+    throw error;
+};
+const onListening = () => {
+    console.log(`API Server is Running on port ${port}`);
+};
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
