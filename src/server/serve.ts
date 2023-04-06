b2b0ad2abc9a4ad53f291237772bf31f991f8b79
YeongT <94744740+YeongT@users.noreply.github.com>
2023-04-07 04:29:07 +0900

    feat: setup eslint to fix code style

--- src/server/serve.ts
+++ src/server/serve.ts
@@ -1,24 +1,22 @@
-import {createServer} from "http";
+import { createServer } from 'http';
 
-//# import express app from routes folder
-import {app} from "../routes/app";
+// # import express app from routes folder
+import app from '../routes/app';
 
 // Get port from environment and store in Express.
-const port = process.env.PORT || 5000;
-app.set("port", port);
+const port = Number(process.env.PORT) || 5000;
+app.set('port', port);
 
 const server = createServer(app);
 const onError = (error: Error) => {
-    if (error.message.includes("EACCES"))
-        throw new Error(`port ${port} requires elevated privileges`);
-    if (error.message.includes("EADDRINUSE"))
-        throw new Error(`port ${port} is already in use`);
-    throw error;
+  if (error.message.includes('EACCES')) { throw new Error(`port ${port} requires elevated privileges`); }
+  if (error.message.includes('EADDRINUSE')) { throw new Error(`port ${port} is already in use`); }
+  throw error;
 };
 const onListening = () => {
-    console.log(`API Server is Running on port ${port}`);
+  console.log(`API Server is Running on port ${port}`);
 };
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
